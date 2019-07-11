9bfb2de57e871a49e6888dcab08ff5b997f05480
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 07:01:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,11 +79,15 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+        :yukihiro_matsumoto] => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
      }
 
     return programmer_hash[:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
+    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
 end
